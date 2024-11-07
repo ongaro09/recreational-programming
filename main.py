36977c90dfd7cd0fd4c3a341a0acaa419b473f96
Ongaro09 <ongaromark7@gmail.com>
2024-11-07 20:22:10 +0300

    Initial commit for fantasy-quest-game

---
+++ main.py
@@ -0,0 +1,25 @@
+"""Welcome to the Fantasy Quest"""
+
+print("Welcome to Fantasy Quest!")
+
+name = "Ongaro"
+sword_damage = 10
+player_health = 1000
+armor_multiplier = 2
+armored_health = player_health * armor_multiplier
+health_after_attack = player_health - sword_damage
+
+
+print(f"{name}'s health is: {player_health}")
+print(f"{name} is hit by a sword for: {sword_damage} damage")
+print(f"{name}'s health is now: {health_after_attack}")
+print("Use the arrow keys to move")
+
+print("Okra : Aye aye!")
+print("Makmende: ...")
+print("Okra: Where are you off to this morning?...")
+print("Makmende : Where did an owl learn to speak?")
+
+print("Starting up game server...")
+print("Local game server is listening on port 8080")
+
